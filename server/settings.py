5617ead7db338d152bb78457ab7bcc7f1b771fdb
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-10-20 16:30:28 -0500

    Update server/settings.py

--- server/settings.py
+++ server/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config('DATABASE_URL')
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
