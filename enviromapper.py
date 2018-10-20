03be882ad0c47ca5263e691fe5156bac64bef0d6
Nick Weiner <bigweiner@ucdavis.edu>
2018-10-20 14:51:35 -0500

    Code now outputs a .json file from a .csv file.

--- enviromapper.py
+++ enviromapper.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-import re
 import pandas as pd
 globe_file = 'GLOBEMeasurementData-9252.csv'
-colnames = ['latitude', 'longitude', 'measured_on', 'nitrates:nitrate and nitrite']
+colnames = [' latitude', ' longitude', ' measured_on', ' nitrates:nitrate and nitrite (mgl)']
 df_ndata = pd.read_csv(globe_file, sep=',', usecols=colnames)
-j_data = df_ndata.to_json()
-print(j_data[0, 300])
+df_ndata.columns = df_ndata.columns.str.strip()
+j_data = df_ndata.to_json('nitrate_place_data.json', orient='records')
+
 
 
 # if __name__ == "__main__":
